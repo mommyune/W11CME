65447b55ac60e0876188a9dea9e312e0bdba7862
une <uneru@tutanota.com>
2023-12-17 15:50:35 +0100

    Just a overall update
    Added ANCII art
    
    Added Colours
    
    Added option to see current context menu option
    
    Added Restart option
    
    Changed Quit from 3 to Q

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
 # W11CME
 Windows 11 Context menu editor.
 
-A simple batch file to change the look of your Windows 11 context (right click) menu.
+A batch file to change the look of your Windows 11 context (right click) menu.
 
 # How to use
 
-Just download the batch file (W11CME.bat)
-
-And then open the batch file (W11CME.bat)
-
-You can use 1, 2 or 3 to go trough the menu
+Download and open the batch file (W11CME.bat)
 
 Press 1 to use the Windows 11 Context menu style
 
 Press 2 to use the Windows 10 Context menu style
 
-Press 3 to close the menu
+Press R to restart the menu
+
+Press Q to close the menu
 
 # Preview
 
-[Windows 10 to Windows 11](https://youtu.be/NNftV_7xvNA)
+Terminal
 
 
-[Windows 11 to Windows 10](https://youtu.be/qA3wvVBMXvQ)
-
-Terminal/cmd
-
-![terminal](https://github.com/mommyune/W11CME/assets/99808967/92d0011f-447d-4461-889d-a5b53273cb3a)
 
 Windows 11 Context menu style
 
--- W11CME.bat
+++ W11CME.bat
@@ -1,56 +1,80 @@
+:restart
 @echo off
 title W11CME
+SetLocal EnableExtensions
+For /F %%G In ('Echo Prompt $E ^| "%__AppDir__%cmd.exe"') Do Set "ESC=%%G"
+echo %ESC%[40m
+
+::: _ __ ___   ___  _ __ ___  _ __ ___  _   _   _   _ _ __   ___ 
+:::| '_ ` _ \ / _ \| '_ ` _ \| '_ ` _ \| | | | | | | | '_ \ / _ \
+:::| | | | | | (_) | | | | | | | | | | | |_| | | |_| | | | |  __/
+:::|_| |_| |_|\___/|_| |_| |_|_| |_| |_|\__, |  \__,_|_| |_|\___|
+:::                                     |___/                    
+
 :begin
-echo Windows 11 Context menu editor
-echo This batch file changes the Windows 11 style of the right click menu
-echo.
-echo Free and opensource made by mommyune
+echo %ESC%[31m
+reg query "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" >nul
+cls
+for /f "delims=: tokens=*" %%R in ('findstr /b ::: "%~f0"') do @echo(%%R
+)
+echo %ESC%[31mFree and opensource made by mommyune
 echo https://mommyune.com
+echo %ESC%[37m
+echo %ESC%[97mWindows 11 Context menu editor
+echo Changes the Windows 11 style of the context (right click) menu.
 echo ========================================================================
-echo Select a option from below:
+if %errorlevel% equ 0 (
+  echo %ESC%[92mWindows 10 Style is enabled
+) else (
+  echo %ESC%[92mWindows 11 Style is enabled
+)
+echo.
+echo %ESC%[97mSelect a option from below:
 echo.
 echo    1) Windows 11 style
-echo       Changes to the default Windows 11 style richt click menu.
+echo       %ESC%[37mChanges to the default Windows 11 style richt click menu.
 echo.
-echo    2) Windows 10 style
-echo       Changes to the older Windows 10 style richt click menu
+echo    %ESC%[97m2) Windows 10 style
+echo       %ESC%[37mChanges to the older Windows 10 style richt click menu
 echo.
-echo    3) Close
-echo       Closes the menu 
+echo    %ESC%[97mR) Restart
+echo       %ESC%[37mRestarts the menu fully
 echo.
+echo    %ESC%[97mQ) Quit
+echo       %ESC%[37mCloses the menu fully 
 echo ========================================================================
-
-set /p op=Type option:
-if "%op%"=="1" goto op1
-if "%op%"=="2" goto op2
-if "%op%"=="3" goto op3
-
-cls
-goto begin
+echo %ESC%[97m
+set /p op=Type option: 
+for %%X in ("q" "Q") do if "%op%"==%%X goto opx
+for %%R in ("r" "R") do if "%op%"==%%R goto opr
+if "%op%"=="1" goto :op1
+if "%op%"=="2" goto :op2
+goto :begin
 
 :op1
 cls
 echo Adding the context menu back
 reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
-timeout 3
+REM
 echo.
 echo Restarting Windows explorer...
 powershell $open_folders = @((New-Object -com shell.application).Windows()).Document.Folder.Self.Path; Stop-Process -Name explorer -Force;  foreach ($element in $open_folders){Invoke-Item $($element)}
-timeout 3
-cls
-goto begin
+REM
+goto :begin
 
 :op2
 cls
 echo Removing the context menu
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-timeout 3
+REM
 echo.
 echo Restarting Windows explorer...
 powershell $open_folders = @((New-Object -com shell.application).Windows()).Document.Folder.Self.Path; Stop-Process -Name explorer -Force;  foreach ($element in $open_folders){Invoke-Item $($element)}
-timeout 3
-cls
-goto begin
+REM
+goto :begin
 
-:op3
+:opr
+goto :restart
+
+:opx
 :End
